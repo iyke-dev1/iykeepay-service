e474e88b9978e7869756d0fa1356b254a1b02a72
Iykeeze113 <151616090+Iykeeze113@users.noreply.github.com>
2024-09-18 22:37:29 +0100

    second commit
    date: 9-18-2024

---
+++ js/style.js
@@ -0,0 +1,61 @@
+function myFunction(){
+    document.getElementById("badboy").innerHTML = "paragragh change.";
+}
+
+
+
+// const shouldSayHi = true;
+
+// if (shouldSayHi == true) {
+//     console.log("Hi there")
+// }
+
+// const stringFive = "5"
+// const intFive = parseInt(stringFive)
+
+
+// console.log(stringFive)
+// console.log(typeof intFive)
+
+// let bankAccountBalance = 500;
+// let costOfItem = prompt("enter the price of the item: ")
+// if (bankAccountBalance > costOfItem){
+//     console.log("Transfer successful!");
+//     bankAccountBalance -= costOfItem
+//     console.log("current balance: " + bankAccountBalance)
+// }
+// else{
+//     alert("insufficient funds!")
+// }
+ 
+ 
+//  let a = 0;
+// let b = 1;
+
+// for  in (a && b) {
+//     a+b
+//     console.log("correct!")
+// }
+
+// let prev2 = 0;
+// let prev1 = 1;
+
+// console.log(prev2)
+// console.log(prev1)
+
+// for (fibo of range(0,18)){
+//     newFibo = prev1 + prev2;
+//     console.log(newFibo);
+//     prev2 = prev1;
+//     prev1 = newFibo;
+// }
+
+// var x = 10
+// console.log(x)
+
+
+// var x = 2
+// console.log(x)
+
+
+// console.log(x)
